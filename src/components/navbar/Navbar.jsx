c8c45b435a001d4511e61ffcae524dc3f0b0b5d0
YDG <zzmn1234@naver.com>
2022-06-20 00:44:00 +0900

    다크모드 구현

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,32 @@
-import './navbar.scss'
-import SearchIcon from '@mui/icons-material/Search';
-import LanguageIcon from '@mui/icons-material/Language';
+import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" placeholder='Search...' />
-          <SearchIcon/>
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
         </div>
         <div className="items">
-          <div className="item">   
+          <div className="item">
             <LanguageIcon className="icon" />
             English
           </div>
           <div className="item">
             <DarkModeOutlinedIcon
-              className="icon"             
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE" })}
             />
           </div>
           <div className="item">
@@ -40,12 +44,16 @@ const Navbar = () => {
             <ListOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            <img src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F38%2Fd9%2Fc6%2F38d9c68ea61be4ddea8e6a87199d6c8d.png&type=sc960_832" alt="" className='avatar' />
+            <img
+              src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F38%2Fd9%2Fc6%2F38d9c68ea61be4ddea8e6a87199d6c8d.png&type=sc960_832"
+              alt=""
+              className="avatar"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

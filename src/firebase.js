da01f728e12f8dcde69faf53d8800152da174d59
YDG <zzmn1234@naver.com>
2022-06-20 16:22:06 +0900

    환경변수 키설정

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "tutorial-c9524.firebaseapp.com",
-  projectId: "tutorial-c9524",
-  storageBucket: "tutorial-c9524.appspot.com",
-  messagingSenderId: "132066345109",
-  appId: "1:132066345109:web:f408e3942e97787a4a1b9c",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAINM,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);

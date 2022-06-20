e735b36da7a6e4d6881eea66fb8619cab36114e5
YDG <zzmn1234@naver.com>
2022-06-20 15:22:12 +0900

    스냅샷 리스너

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -13,11 +13,13 @@ import {
 import { auth, db, storage } from "../../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
   const [data, setData] = useState({});
   const [per, setPerc] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const uploadFile = () => {
@@ -76,6 +78,7 @@ const New = ({ inputs, title }) => {
         ...data,
         timeStamp: serverTimestamp(),
       });
+      navigate(-1);
     } catch (err) {
       console.log(err);
     }
